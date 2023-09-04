dc5750d99d10f8b19838254beb7b1bd643e335a4
Leegeonmin <kmlee97s@gmail.com>
2023-09-04 15:01:30 +0900

    test : 빈 이름 및 클래스 조회 테스트 코드 작성

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,19 +7,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
-        
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+//
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
         Long MemberId =1L;
         Member member = new Member(MemberId, "memberA", Grade.VIP);
         memberService.join(member);
         
-        Order order = orderService.createOrder(MemberId, "itemA", 10000);
+        Order order = orderService.createOrder(MemberId, "itemA", 20000);
         System.out.println("order.toString() = " + order.toString());
         System.out.println("order.toString() = " + order.calculatePrice());
     }
