053ee21daabe9028a34c91a2d57a6afdf847977f
Leegeonmin <kmlee97s@gmail.com>
2023-09-11 15:00:38 +0900

    feat : @Autowired 필드명, Qualifier, Primary로 스프링 빈 타입 중복 문제 해결 코드 작성

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,23 +3,30 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements  OrderService{
 
     //필드에 바로 의존성 주입
     // 필드 주입은 간결하지만 외부에서 변경 불가능, 테스트 힘듦
     //@Autowired
     private final MemberRepository _memberRepository;
-    //@Autowired
+//    @Autowired
     private final DiscountPolicy _discountPolicy;
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        _memberRepository = memberRepository;
-//        _discountPolicy = discountPolicy;
-//    }
+    
+    // 필드명을 매칭시켜주면 같은 타입이 Component로 스프링빈에 등록되어도 필드명으로 매칭시켜준다
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository,  DiscountPolicy discountPolicy) {
+        _memberRepository = memberRepository;
+        _discountPolicy = discountPolicy;
+    }
+    public MemberRepository getMemberRepository(){
+        return this._memberRepository;
+    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = _memberRepository.findById(memberId);
