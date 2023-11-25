588a27c27f3c30324226dab75d97fa2b9c55b37d
Leegeonmin <kmlee97s@gmail.com>
2023-11-25 22:22:22 +0900

    feat : ObjectProvider를 사용해 Request 스코프인 bean 사용가능하도록 변경 (스프링 컨테이너에서 빈을 등록하고 의존관계를 설정할 때 Request 빈은 등록이 되어있지 않아 생기는 문제)

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -3,12 +3,14 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.ObjectProvider;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String Id){
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + Id);
     }
 }
